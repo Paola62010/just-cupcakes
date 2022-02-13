220d258216a4dc1a11cc63bfac73d6ad77d8ed36
Paola <paola62010@gmail.com>
2022-02-13 12:47:21 +0000

    Add favicon

--- justcupcakes/urls.py
+++ justcupcakes/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', include('store.urls'), name= 'store.urls'), 
+    path('', include('store.urls'), name= 'store.urls'),
+    path(
+        'favicon.ico',
+        RedirectView.as_view(url=staticfiles_storage.url('favicon.ico')),
+    ),
 ]
