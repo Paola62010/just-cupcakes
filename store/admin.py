2d2f1f7262b1932403b099660b10868a89f5900b
Paola <paola62010@gmail.com>
2022-02-19 17:52:54 +0000

    Customize admin view for product and category models

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Product, Category
 
 # Register your models here.
+admin.site.site_header = 'Just Cupcakes Administration'
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'created_on', 'updated_on',)
+    list_filter = ('created_on', 'updated_on',)
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'category', 'sku', 'price',
+                    'created_on', 'updated_on',)
+    list_filter = ('created_on', 'updated_on',)
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name', 'created_by__username',)
+    exclude = ('created_by',)
