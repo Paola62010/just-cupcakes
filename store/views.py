e23ff2cb600e201e574f94efd782ea9b3fe0f886
Paola <paola62010@gmail.com>
2022-02-20 19:13:04 +0000

    Add view_product view, url and template. Add structure and style to template

--- store/views.py
+++ store/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from .models import Product, Category
 
-# Create your views here.
 
 def home(request):
     """A view to render the home page"""
     return render(request, 'index.html')
+
+
+def view_products(request):
+    """A view to show all available products"""
+
+    products = Product.objects.all()
+    context = {
+        'products': products
+    }
+    return render(request, 'products.html', context)
