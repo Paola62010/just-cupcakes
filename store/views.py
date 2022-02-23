94ffbc565312ace375650c9dddd4e2c20be0b4d0
Paola <paola62010@gmail.com>
2022-02-23 19:45:06 +0000

    Add sort functionality

--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
 from django.db.models import Q
+from django.db.models.functions import Lower
 
 
 def home(request):
@@ -13,16 +14,43 @@ def view_products(request):
 
     products = Product.objects.all()
     query = None
+    categories = None
+    current_sorting = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
         if 'q' in request.GET:
             query = request.GET['q']
             result = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(result)
 
+        if 'category' in request.GET:
+            categories = request.GET['category']
+            products = products.filter(category__slug=categories)
+            categories = Category.objects.filter(slug=categories)
+
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
-        'search': query
+        'search': query,
+        'current_category': categories,
+        'current_sorting': current_sorting
     }
     return render(request, 'products.html', context)
 
