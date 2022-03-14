e95fc5ba78878b2be0cd5bb6c09bb3a7217c4d6f
Paola <paola62010@gmail.com>
2022-03-14 19:19:28 +0000

    Add stripe card element and correct style to have all inputs match

--- checkout/views.py
+++ checkout/views.py
@@ -16,6 +16,8 @@ def checkout(request):
     template = 'checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KKhJrHRkr0tI58irir9rZlbjbGT0wG2MoL2EQ2ynstvSylWUhnAl2naY5JcEKuaykcmD6DOMIDUOVyz91y5AFYB00IQBN0jUI',
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)
