db4a0fc32bc0bf8b6c082c5dd63842d5076debb3
Paola <paola62010@gmail.com>
2022-03-17 19:34:57 +0000

    Add basic functionlity to handle stripe payments in checkout view and js to handle checkout form submission

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,6 +1,6 @@
-var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-var client_secret = $('#id_client_secret').text().slice(1, -1);
-var stripe = Stripe(stripe_public_key);
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 var style = {
     base: {
@@ -36,3 +36,32 @@ card.addEventListener('change', function(event){
         errorDiv.textContent = '';
     }
 });
+
+// Checkout form submission
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    card.update({'disabled': true});
+    $('#order-submit').attr('disabled', true);
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result){
+        if (result.error){
+            var errorDiv = document.getElementById('card-errors');
+            var htmlContent = `
+                <span>
+                    <i class="bi bi-exclamation-triangle-fill"></i>
+                </span>
+                <span>${event.error.message}</span>`;
+            $(errorDiv).html(htmlContent);
+            card.update({'disabled': false});
+            $('#order-submit').attr('disabled', false);
+        } else {
+            if (result.paymentIntent.status === 'succeeded'){
+                form.submit()
+            }
+        }
+    })
+})
--- checkout/views.py
+++ checkout/views.py
@@ -2,22 +2,38 @@ from re import template
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.contrib import messages
+from django.conf import settings
 from .models import Order, LineItem
 from .forms import OrderForm
+from bag.contexts import bag_content
+import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, 'There is nothing in your bag yet.')
         return redirect(reverse('products'))
 
+    current_bag = bag_content(request)
+    order_total = current_bag['grand_total']
+    stripe_total = round(order_total * 100)
+    stripe.api_key = stripe_secret_key
+    payment_intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY
+    )
+
+    print(payment_intent)
+
     order_form = OrderForm()
     template = 'checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51KKhJrHRkr0tI58irir9rZlbjbGT0wG2MoL2EQ2ynstvSylWUhnAl2naY5JcEKuaykcmD6DOMIDUOVyz91y5AFYB00IQBN0jUI',
-        'client_secret': 'test client secret'
+        'stripe_public_key': stripe_public_key,
+        'client_secret': payment_intent.client_secret
     }
 
     return render(request, template, context)
