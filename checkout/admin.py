d233278c6756bbfca6d536ac56e5e44098afcc62
Paola <paola62010@gmail.com>
2022-03-22 11:52:16 +0000

    Complete webhook handling logic

--- checkout/admin.py
+++ checkout/admin.py
@@ -12,12 +12,14 @@ class OrderAdmin(admin.ModelAdmin):
     inlines = (LineItemAdminInline,)
     readonly_fields = ('order_number', 'order_date',
                        'delivery_cost', 'order_total',
-                       'grand_total')
+                       'grand_total', 'original_bag',
+                       'stripe_pid')
     fields = ('order_number', 'full_name', 'email',
               'phone', 'city', 'address_line1',
               'address_line2', 'post_code', 'county',
               'country', 'order_date', 'delivery_cost',
-              'order_total', 'grand_total')
+              'order_total', 'grand_total', 'original_bag',
+              'stripe_pid')
     list_display = ('order_number', 'full_name', 'order_date',
                     'delivery_cost', 'order_total', 'grand_total')
     ordering = ('-order_date',)
